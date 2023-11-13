38273959859ef31a1f01f03c3836610327ef6b5c
dvub <dvubdevs@gmail.com>
2023-11-12 20:58:16 -0800

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,30 @@
+'use client';
+import axios from 'axios';
+import { ChangeEvent, useState } from 'react';
+import useSWR from 'swr';
+import Select, { ActionMeta, SingleValue } from 'react-select';
+
 export default function Home() {
+  
+  const [location, setLocation] = useState("");
+
+
+  const key = process.env.NEXT_PUBLIC_OPENWEATHERMAP_KEY;
+  const limit = 3;
+
+  const geoFetcher = (url: string) => axios.get(url).then(res => res.data)
+
+  const { data: locationData, error: locationError, isLoading: locationIsLoading } = useSWR(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${"reno"}&limit=${limit}&appid=${key}`,
+    geoFetcher
+  );
+
+  const onLocationChange = (newValue: SingleValue<string>, actionMeta: ActionMeta<string>) => {
+    setLocation(newValue!);
+  }
+
   return (
-    <main className="">
+    <main>
       <div>
         <h1>Hello world</h1>
       </div>
