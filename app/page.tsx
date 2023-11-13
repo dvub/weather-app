9c855d40088b4ee7999a5f2bed531c30f4fb9693
dvub <dvubdevs@gmail.com>
2023-11-12 21:28:52 -0800

    FUCK

--- app/page.tsx
+++ app/page.tsx
@@ -5,27 +5,25 @@ import useSWR from 'swr';
 import Select, { ActionMeta, InputActionMeta, SingleValue } from 'react-select';
 
 export default function Home() {
-  const locationQueryLimit = 5;
-  const key = process.env.NEXT_PUBLIC_OPENWEATHERMAP_KEY;
-
+  
   const [location, setLocation] = useState({
     searchTerm: '',
     options: []
   });
 
-  const onLocationChange = (newValue: string, actionMeta: InputActionMeta) => {
+  const key = process.env.NEXT_PUBLIC_OPENWEATHERMAP_KEY;
+  const limit = 3;
 
+  const geoFetcher = (url: string) => axios.get(url).then(res => res.data);
+  
+  const {data, isLoading, error } = useSWR(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${"reno"}&limit=${limit}&appid=${key}`,
+    geoFetcher
+  );
+
+  const onLocationChange = (newValue: string, actionMeta: InputActionMeta) => {
     setLocation({...location, searchTerm: newValue});
-    axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${newValue}&limit=${locationQueryLimit}&appid=${key}`, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(data => {
-      const options = data.data.map((location: any) => {
-        return {label: location.city, value: location}
-      })
-      setLocation({...location, options: options});
-    });
+
   }
 
   return (
