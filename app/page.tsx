dd56ade5249875c6eddf4c4c51c1755f4c29310e
dvub <dvubdevs@gmail.com>
2023-11-12 21:35:34 -0800

    WHAT

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { ChangeEvent, useState } from 'react';
 import useSWR from 'swr';
 import Select, { ActionMeta, InputActionMeta, SingleValue } from 'react-select';
+import useLocation from './hooks/useLocation';
 
 export default function Home() {
   
@@ -11,19 +12,9 @@ export default function Home() {
     options: []
   });
 
-  const key = process.env.NEXT_PUBLIC_OPENWEATHERMAP_KEY;
-  const limit = 3;
-
-  const geoFetcher = (url: string) => axios.get(url).then(res => res.data);
-  
-  const {data, isLoading, error } = useSWR(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${"reno"}&limit=${limit}&appid=${key}`,
-    geoFetcher
-  );
-
   const onLocationChange = (newValue: string, actionMeta: InputActionMeta) => {
     setLocation({...location, searchTerm: newValue});
-
+    useLocation(newValue)
   }
 
   return (
