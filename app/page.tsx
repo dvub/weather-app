87e8510a544a5d56a93710e33cdc483e17c05b80
dvub <dvubdevs@gmail.com>
2023-11-14 10:41:27 -0800

    more progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,41 @@
-'use client';
-import LocationSelector from '@/components/locationSelector';
-import axios from 'axios';
-import React from 'react';
-import { useState } from 'react';
-import Select, { InputActionMeta } from 'react-select';
+'use client'
+import LocationSelector from '@/components/locationSelector'
+import axios from 'axios'
+import React from 'react'
+import { useState } from 'react'
+import Select, { InputActionMeta } from 'react-select'
 
 export default function Home() {
-	const [location, setLocation] = useState({
-		searchTerm: '',
-		options: [],
-	});
-	const key = process.env.NEXT_PUBLIC_OPENWEATHERMAP_KEY;
-	const endpoint = process.env.NEXT_PUBLIC_GEO_ENDPOINT;
-	const limit = 3;
-
-	const updateLocationOptions = (query: string) => {
-		axios
-			.get(`${endpoint}?q=${query}&limit=${limit}&appid=${key}`)
-			.then((response) => {
-				const options = response.data.map((location: any) => {
-					return {
-						label: `${location.name}, ${location.state}`,
-						value: location,
-					};
-				});
-				setLocation({ ...location, options: options });
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
-
-	const onLocationChange = (
-		newValue: string,
-		actionMeta: InputActionMeta
-	) => {
-		if (newValue === undefined) {
-			return;
-		}
-		setLocation({ ...location, searchTerm: newValue });
-	};
-
+	const [coordinates, setCoordinates] = React.useState({
+		latitude: 0,
+		longitude: 0,
+	})
+	// whenever the LocationSelector componenet updates the coordinates,
+	// we will run this code - this should make an API request now that we have the coords
+	//
 	React.useEffect(() => {
-		const getDataTimeout = setTimeout(() => {
-			updateLocationOptions(location.searchTerm);
-		}, 2000);
-
-		return () => clearTimeout(getDataTimeout);
-	}, [location.searchTerm]);
+		console.log(coordinates)
+		getWeatherData(coordinates.latitude, coordinates.longitude)
+	}, [coordinates])
+	const endpoint = process.env.NEXT_PUBLIC_WEATHER_ENDPOINT
+	const key = process.env.NEXT_PUBLIC_OPENWEATHERMAP_KEY
+	const getWeatherData = (latitude: number, longitude: number) => {
+		axios
+			.get(`${endpoint}?lat=${latitude}&lon=${longitude}&appid=${key}`)
+			.then((response) => {
+				console.log(response)
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}
 
 	return (
 		<main>
 			<div>
 				<h1>Hello world</h1>
 			</div>
-			<LocationSelector setCoordinates={''} />
+			<LocationSelector setCoordinates={setCoordinates} />
 		</main>
-	);
+	)
 }
--- components/locationSelector.tsx
+++ components/locationSelector.tsx
@@ -1,61 +1,68 @@
-import axios from 'axios';
-import React from 'react';
-import Select, { ActionMeta, InputActionMeta } from 'react-select';
+import axios from 'axios'
+import React from 'react'
+import Select, { ActionMeta, InputActionMeta } from 'react-select'
 
-export default function LocationSelector(props: { setCoordinates: any }) {
+export default function LocationSelector(props: {
+	setCoordinates: React.Dispatch<
+		React.SetStateAction<{
+			latitude: number
+			longitude: number
+		}>
+	>
+}) {
 	const [location, setLocation] = React.useState({
 		searchTerm: '',
 		options: [],
-	});
-	const searchRef = React.useRef<string>('');
-	let selectRef: React.RefObject<Select> = React.createRef();
-
+	})
+	const searchRef = React.useRef<string>('')
 	React.useEffect(() => {
 		const getDataTimeout = setTimeout(() => {
-			updateLocationOptions(searchRef.current);
-		}, debounceTimeout);
+			updateLocationOptions(searchRef.current)
+		}, debounceTimeout)
 
-		return () => clearTimeout(getDataTimeout);
-	}, [location.searchTerm]);
+		return () => clearTimeout(getDataTimeout)
+	}, [location.searchTerm])
 
 	// get stuff from env variables about our api
-	const key = process.env.NEXT_PUBLIC_OPENWEATHERMAP_KEY;
-	const endpoint = process.env.NEXT_PUBLIC_GEO_ENDPOINT;
-	const queryLimit = 3;
+	const key = process.env.NEXT_PUBLIC_OPENWEATHERMAP_KEY
+	const endpoint = process.env.NEXT_PUBLIC_GEO_ENDPOINT
+	const queryLimit = 3
 	// minimum time for each location option update
 	// this might get rate-limited at a certain timeout, so this won't really do much
-	const debounceTimeout = 500;
+	const debounceTimeout = 500
 
 	const updateLocationOptions = (query: string) => {
 		// don't make an empty query
-		if (!query) return;
+		if (!query) return
 		// use axios to make a get request to the endpoint
 		axios
 			.get(`${endpoint}?q=${query}&limit=${queryLimit}&appid=${key}`)
 			.then((response) => {
-				console.log(response.data);
 				const options = response.data.map((location: any) => {
 					return {
 						label: `${location.name}, ${location.state}`,
 						value: location,
-					};
-				});
-				setLocation({ ...location, options: options });
+					}
+				})
+				setLocation({ ...location, options: options })
 			})
 			.catch((err) => {
-				console.log(err);
-			});
-	};
+				console.log(err)
+			})
+	}
 
 	const onSearchChange = (newValue: string, actionMeta: InputActionMeta) => {
-		searchRef.current = newValue;
-		setLocation({ ...location, searchTerm: newValue });
-	};
+		searchRef.current = newValue
+		setLocation({ ...location, searchTerm: newValue })
+	}
 
-	const onLocationChange = (
-		newValue: null,
-		actionMeta: ActionMeta<never>
-	) => {};
+	const onLocationChange = (newValue: any, actionMeta: ActionMeta<never>) => {
+		const { lat, lon } = newValue.value
+		props.setCoordinates({
+			latitude: lat,
+			longitude: lon,
+		})
+	}
 	return (
 		<Select
 			isSearchable={true}
@@ -63,9 +70,6 @@ export default function LocationSelector(props: { setCoordinates: any }) {
 			inputValue={searchRef.current}
 			onInputChange={onSearchChange}
 			placeholder={'Type a location to search...'}
-			onChange={onLocationChange}
-			loadingMessage={() => 'loading'}
-			ref={(_) => selectRef}
-		></Select>
-	);
+			onChange={onLocationChange}></Select>
+	)
 }
