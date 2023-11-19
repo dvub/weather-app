c1813a852e81d7696c3bdc3d42f43ef39d760195
dvub <dvubdevs@gmail.com>
2023-11-18 16:52:23 -0800

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 'use client';
+import LocalTimeDisplay from '@/components/LocalTimeDisplay';
 import LocationSelector from '@/components/locationSelector';
-import WeatherBackground from '@/components/WeatherBackground';
-import WeatherDisplay from '@/components/WeatherDisplay';
+import MiscDisplay from '@/components/MiscDisplay';
+import OverviewDisplay from '@/components/OverviewDisplay';
+import WindDisplay from '@/components/WindDisplay';
 import { WeatherAPIResponse } from '@/types/';
 import axios from 'axios';
 import React from 'react';
@@ -38,12 +40,48 @@ export default function Home() {
 				console.log(error);
 			});
 	};
+	// if we can't get a location or whatever
+	if (weather === undefined) {
+		return (
+			<div>
+				<LocationSelector setCoordinates={setCoordinates} />
+				<p>No weather found :\</p>
+			</div>
+		);
+	}
+	const clamp = (num: number, min: number, max: number) =>
+		Math.min(Math.max(num, min), max);
+
+	// get times in milliseconds
+	const msTime = Date.now();
+	const sunrise = new Date(parseInt(weather.sys.sunrise) * 1000).getTime();
+	const sunset = new Date(parseInt(weather.sys.sunset) * 1000).getTime();
+
+	// get the difference in hours (rounded down) and take the minimum one
+	const sunriseDiff = Math.floor(Math.abs(sunrise - msTime) / 3600000);
+	const sunsetDiff = Math.floor(Math.abs(sunset - msTime) / 3600000);
+	const isDay = msTime > sunrise && msTime < sunset;
+	// if it's night, multiply the minimum of diff between sunrise/set by -1,
+	// so that the base variant is subtracted from
+	const minDifference = Math.min(sunriseDiff, sunsetDiff) * (isDay ? 1 : -1);
+	// 6 is the variant that looks like sunrise/sunset most to me, imo
+	// so that's the baseline and we will add or subtract
+	const variant = 6 + minDifference;
+	const clamped = clamp(variant, 1, 11);
 
 	return (
-		<main>
-			<LocationSelector setCoordinates={setCoordinates} />
-			<WeatherDisplay weather={weather} />
-			<p>TODO: metric toggle</p>
+		<main className='bg-gray-900 h-screen overflow-hidden'>
+			<div
+				className={`sky-gradient-${clamped} m-5 p-5 rounded-md text-white`}
+			>
+				<LocationSelector setCoordinates={setCoordinates} />
+				<OverviewDisplay weather={weather!} />
+				<div className='mt-5 other flex gap-3'>
+					<WindDisplay weather={weather!} />
+					<LocalTimeDisplay weather={weather!} />
+					<MiscDisplay weather={weather!} />
+				</div>
+			</div>
 		</main>
 	);
 }
